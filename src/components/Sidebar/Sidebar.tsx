b4bac020be0d8a5e8b40009d704c966c90cb1bc0
gungyusma <61109894+gungyusma@users.noreply.github.com>
2023-09-05 21:44:07 +0800

    fixing logos

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -14,7 +14,7 @@ function Sidebar() {
 
       <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-          MENU
+          
             <ul className="space-y-2 font-medium">
               <li>
                   <Link to="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
